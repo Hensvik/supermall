298e8789941f140e59055bdd082a5b4e9c7ad4c0
冯卓能 <fengzhuoneng@fufluns.net>
2021-02-23 00:11:05 +0800

    all

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,38 +1,38 @@
 <template>
   <div id="home">
-    <!--<nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banners="banners"/>
+    <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
+    <!--<home-swiper :banners="banners"/>
     <recommend-view :recommends="recommends"/>-->
   </div>
 </template>
 
 <script>
-  //import NavBar from 'components/common/navbar/NavBar';
+  import NavBar from 'components/common/navbar/NavBar';
   // import HomeSwiper from './childComps/HomeSwiper'
   // import RecommendView from './childComps/RecommendView'
   //
-  // import {getHomeMultidata} from "network/home";
+   import {getHomeMultidata} from "network/home";
 
   export default {
     name: "Home",
     components: {
-      // NavBar,
+       NavBar,
       // HomeSwiper,
       // RecommendView
     },
     data() {
       return {
-        // banners: [],
-        // recommends: []
+         banners: [],
+         recommends: []
       }
     },
     created() {
       // 1.请求多个数据
-      // getHomeMultidata().then(res => {
-      //   // this.result = res;
-      //   this.banners = res.data.banner.list;
-      //   this.recommends = res.data.recommend.list;
-      // })
+      getHomeMultidata().then(res => {
+        // this.result = res;
+        this.banners = res.data.banner.list;
+        this.recommends = res.data.recommend.list;
+      })
     }
   }
 </script>
