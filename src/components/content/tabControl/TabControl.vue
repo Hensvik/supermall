d52177509284009822a61490f6f0f562ca2d0c94
冯卓能 <fengzhuoneng@fufluns.net>
2021-02-25 00:18:12 +0800

    all

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="tab-control">
+      <div v-for="(item,index) in titles" class="tab-control-item" :key="item" :class="{active:index === currentIndex}">
+      <span>{{item}}</span>
+    </div>
+    <div><span></span></div>
+    <div><span></span></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "tabControl",
+    props:{
+      titles:{
+        type:Array,
+        default(){
+          return []
+        }
+      }
+    },
+
+  }
+</script>
+
+<style scoped>
+  .tab-control{
+    display:flex;
+    text-align:center;
+    font-size:15px;
+    height:40px;
+    line-height:40px;
+  }
+
+  .tab-control-item{
+    flex:1;
+  }
+
+  .tab-control-item span{
+    padding:5px;
+  }
+
+  .active{
+    color:var(--color-high-text);
+  }
+
+  .active span{
+    border-bottom: 3px solid var(--color-tint);
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,34 +1,47 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <!--<home-swiper :banners="banners"/>
-    <recommend-view :recommends="recommends"/>-->
-    <swiper>
-      <swiper-item v-for="item in banners">
-        <a :href="item.link">
-          <img :src="item.image" alt="">
-        </a>
-      </swiper-item>
-    </swiper>
+    <home-swiper :banners="banners"/>
+    <recommend-view :recommends="recommends"/>
+    <feature-view></feature-view>
+
+    <tab-control :titles="['流行','新款','精选']"></tab-control>
+
+    <ul>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+      <li>列表</li>
+    </ul>
   </div>
 </template>
 
 <script>
+  import HomeSwiper from './childComps/HomeSwiper';
+  import RecommendView from './childComps/RecommendView';
+  import FeatureView from './childComps/FeatureView';
+
   import NavBar from 'components/common/navbar/NavBar';
-  // import HomeSwiper from './childComps/HomeSwiper'
-  // import RecommendView from './childComps/RecommendView'
-  //
-  import {Swiper,SwiperItem} from '@/components/common/swiper';
-   import {getHomeMultidata} from "@/network/home";
+  import TabControl from 'components/content/tabControl/TabControl';
+
+  import {getHomeMultidata} from "@/network/home";
 
   export default {
     name: "Home",
     components: {
-       NavBar,
-      Swiper,
-      SwiperItem
-      // HomeSwiper,
-      // RecommendView
+      HomeSwiper,
+      RecommendView,
+      FeatureView,
+      NavBar,
+      TabControl
     },
     data() {
       return {
@@ -49,8 +62,18 @@
 </script>
 
 <style scoped>
+  #home {
+    padding-top:44px;
+  }
+
   .home-nav {
     background-color: var(--color-tint);
     color: #fff;
+
+    position:fixed;
+    left:0;
+    right:0;
+    top:0;
+    z-index:9;
   }
 </style>
